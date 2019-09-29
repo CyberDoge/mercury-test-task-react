a1d0aab7b084cb847ae29b631e00063a4a907bc6
Tagir <tagirman998@gmail.com>
2019-09-30 00:11:39 +0400

    fix updating class

--- src/components/centralBlock/formBlock/formInputItem/FormInputItem.js
+++ src/components/centralBlock/formBlock/formInputItem/FormInputItem.js
@@ -3,11 +3,14 @@ import "components/centralBlock/formBlock/formInputItem/FormInputItem.css";
 import classJoiner from "../../../../service/ComponentService";
 
 export default class FormInputItem extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.typeClass = classJoiner(this.props.typeClass);
+  }
   render() {
-    const typeClass = classJoiner(this.props.typeClass)
     return (
       <input
-        className={`form__input-item ${typeClass}`}
+        className={`form__input-item ${this.typeClass}`}
         {...this.props}
       />
     );
